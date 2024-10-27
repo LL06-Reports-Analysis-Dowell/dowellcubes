4d1568d903a146dd923f4a14402405bdf5f05adb
Manish <mdashsharma95@gmail.com>
2024-10-27 10:59:41 +0530

    fix: Frontend route

--- client/src/services/api.services.js
+++ client/src/services/api.services.js
@@ -2,11 +2,11 @@ import { servicesAxiosInstance } from './config';
 
 
 export const getServerStatus = async () => {
-  const response = await servicesAxiosInstance.get('/api/v1/self');
+  const response = await servicesAxiosInstance.get('/v1/self');
   return response.data;
 };
 
 export const getServerHealth = async () => {
-  const response = await servicesAxiosInstance.get('/api/v1/health');
+  const response = await servicesAxiosInstance.get('/v1/health');
   return response.data;
 };
